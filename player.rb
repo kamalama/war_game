acbb91a280e40229e5859681520071fa7447c8ab
Kamla Kasichainula <kasikamla@gmail.com>
2016-07-04 14:57:16 -0700

    add some pseudocode for handing shuffling and triggering game end if the collection is empty

--- player.rb
+++ player.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+
 class Player
   def initialize(name, game_data)
     @name = name
@@ -20,8 +21,24 @@ class Player
   end
 
   def play_card
-    #if @deck.empty? shuffle_collection
+    if @deck.empty?
+      shuffle_collection
+      #if the collection is empty after shuffling, trigger game_end
+    end
+
     @deck.shift
+  end
+
+  def shuffle_collection
+    if @collection.empty?
+      return []
+    else
+      get_shuffled_collection
+    end
+  end
+
+  def get_shuffled_collection
+    #makes request to server for shuffled deck
   end
 
   def collect_cards(cards)
